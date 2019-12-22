b95c892f5785e22e279a139a7c1ecc511a44c20e
chenxu <chenxu@mail.ustc.edu.cn>
2019-12-22 16:27:17 +0800

    release cnpy with cxwx account

--- Formula/cnpy.rb
+++ Formula/cnpy.rb
@@ -1,8 +1,9 @@
 class Cnpy < Formula
   desc "C++ io of numpy"
   homepage "https://github.com/rogersce/cnpy"
-  url "https://github.com/rogersce/cnpy/archive/master.tar.gz"
-  version "0.0.1"
+#  url "https://github.com/rogersce/cnpy/archive/master.tar.gz"
+  url "https://github.com/cxwx/cnpy/archive/V0.1.0.tar.gz"
+#  version "0.1.0"
 
 
   depends_on "cmake" => [:build, :test]
