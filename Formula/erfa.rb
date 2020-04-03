2a1bf7e8c767fca5afd0871a86fd13262b721abb
chenxu <chenxu@mail.ustc.edu.cn>
2020-04-03 15:32:13 +0800

    add erfa

---
+++ Formula/erfa.rb
@@ -0,0 +1,14 @@
+class Erfa < Formula
+  desc "Essential Routines for Fundamental Astronomy."
+  homepage "https://github.com/liberfa/erfa"
+  url "https://github.com/liberfa/erfa/releases/download/v1.7.0/erfa-1.7.0.tar.gz"
+  sha256 "f0787e30e848750c0cbfc14827de6fc7f69a2d5ef0fc653504e74b8967a764e0"
+
+  def install
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--prefix=#{prefix}"
+    system "make", "install"
+  end
+
+end
