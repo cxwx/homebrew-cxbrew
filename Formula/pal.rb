d1552c3ab36c003ceed5f60818831ca007c64d1d
chenxu <chenxu@mail.ustc.edu.cn>
2020-04-06 17:31:21 +0800

    upgrade pal

--- Formula/pal.rb
+++ Formula/pal.rb
@@ -3,6 +3,9 @@ require "formula"
 class Pal < Formula
 
   homepage "https://github.com/IceCube-SPNO/pal"
+  version "0.9.8"
+  url "https://github.com/Starlink/pal/releases/download/v0.9.8/pal-0.9.8.tar.gz"
+  sha256 "191fde8c4f45d6807d4b011511344014966bb46e44029a4481d070cd5e7cc697"
 
   depends_on "erfa"
   # batteries not included
@@ -13,10 +16,11 @@ class Pal < Formula
   end
 
   def install
-    system "autoreconf", "--install", "--symlink", buildpath
+#    system "autoreconf", "--install", "--symlink", buildpath
     system "./configure", "--disable-debug",
-                          "--disable-dependency-tracking",
-                          "--disable-silent-rules",
+                          "--without-starlink",
+#                          "--disable-dependency-tracking",
+#                          "--disable-silent-rules",
                           "--prefix=#{prefix}"
     system "make", "install"
   end
