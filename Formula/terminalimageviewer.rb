083216a24446020bcd7497a3d56af19552ebc084
chenxu <chenxu@mail.ustc.edu.cn>
2020-05-18 20:26:41 +0800

    tiv usable

--- Formula/terminalimageviewer.rb
+++ Formula/terminalimageviewer.rb
@@ -1,8 +1,8 @@
 class Terminalimageviewer < Formula
   desc "Display images in a terminal using block graphic characters"
   homepage "https://github.com/stefanhaustein/TerminalImageViewer"
-  url "https://github.com/stefanhaustein/TerminalImageViewer/archive/v1.0.0.tar.gz"
-  sha256 "d28c5746d25d83ea707db52b54288c4fc1851c642ae021951967e69296450c8c"
+  url "https://github.com/cxwx/TerminalImageViewer/archive/1.0.1.tar.gz"
+  sha256 "ed46a1806dec82dd89dd3f9174ff74568b0a912153c7615a1b952104a353e648"
   head "https://github.com/stefanhaustein/TerminalImageViewer.git"
   depends_on "gcc" => :build unless OS.linux?
   depends_on "imagemagick"
@@ -12,16 +12,16 @@ class Terminalimageviewer < Formula
       system "make"
       if OS.mac?
         # No expermimental/filesystem.h on mac
-        system "#{Formula["gcc"].opt_bin}/gcc-#{Formula["gcc"].version_suffix}", "-std=c++17",
-                                                                                  "-Wall",
-                                                                                  "-L/usr/local/opt/gcc/lib/gcc/9/",
-                                                                                  "-fpermissive",
-                                                                                  "-fexceptions",
-                                                                                  "-O2", "-c",
-                                                                                  "tiv.cpp", "-o", "tiv.o"
-        system "#{Formula["gcc"].opt_bin}/gcc-#{Formula["gcc"].version_suffix}", "tiv.o", "-o", 
-                                                                                 "tiv", "-L/usr/local/opt/gcc/lib/gcc/9/ -lstdc++fs",
-                                                                                 "-pthread", "-s"
+        # system "#{Formula["gcc"].opt_bin}/gcc-#{Formula["gcc"].version_suffix}", "-std=c++17",
+        system "c++", "-std=c++17",
+                      "-Wall",
+                      "-fpermissive",
+                      "-fexceptions",
+                      "-O2", "-c",
+                      "tiv.cpp", "-o", "tiv.o"
+        system "c++", "tiv.o", "-o", 
+                      "tiv", "-L/usr/local/opt/gcc/lib/gcc/9/", "-lstdc++fs",
+                      "-pthread", "-s"
       else
         system "make"
       end
