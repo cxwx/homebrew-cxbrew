d11b6cbe1770d58906f6f27ec7d0da430a8304ac
chenxu <chenxu@mail.ustc.edu.cn>
2020-05-24 20:27:44 +0800

    brew audit --strict

--- Formula/physunits.rb
+++ Formula/physunits.rb
@@ -1,7 +1,7 @@
 class Physunits < Formula
-  desc "A small C++11, C++14 header-only library for compile-time dimensional analysis and unit/quantity manipulation and conversion"
+  desc "C++ header-only for Physics unit/quantity manipulation and conversion"
   homepage "https://github.com/martinmoene/PhysUnits-CT-Cpp11"
-  url "https://codeload.github.com/martinmoene/PhysUnits-CT-Cpp11/zip/master"
+  url "https://github.com/martinmoene/PhysUnits-CT-Cpp11/archive/master.tar.gz"
   version "1.1.0"
   sha256 "8a4974e816e576f55e5e6b27874c364ae6181405911b864edd0912402c3066ae"
 
@@ -35,5 +35,4 @@ class Physunits < Formula
     system ENV.cxx, "-std=c++11", "test.cpp", "-o", "test", "-I#{include}"
     assert_match /12.5/, shell_output("./test")
   end
-
 end
