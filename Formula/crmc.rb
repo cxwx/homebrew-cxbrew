79f75fb022fac529da21b2e3d2c962fe06c36f94
chenxu <chenxu@mail.ustc.edu.cn>
2020-07-03 22:43:16 +0800

    crmc 1.7.0->1.7.1

--- Formula/crmc.rb
+++ Formula/crmc.rb
@@ -1,16 +1,18 @@
 class Crmc < Formula
   desc "Cosmic Ray Monte Carlo"
   homepage "https://devel-ik.fzk.de/wsvn/mc/crmc/"
-  url "https://github.com/alisw/crmc/archive/v1.7.0.tar.gz"
-  sha256 "59086f4e654d775a4f6c3974ae89bbfd995391c4677f266881604878b47563d1"
+#  url "https://github.com/alisw/crmc/archive/v1.7.0.tar.gz"
+  url "https://gitlab.ikp.kit.edu/AirShowerPhysics/crmc/-/archive/svn/crmc.v1.7.1/crmc-svn-crmc.v1.7.1.tar.gz" 
 
   depends_on "cmake" => :build
-  depends_on "gcc" 
+  depends_on "gcc@9" 
+  # depends_on "gcc" # gcc 10 bug
 
   def install
     mkdir "../build" do
       args = %W[
         -DCMAKE_INSTALL_PREFIX=#{prefix}
+        -DCMAKE_Fortran_COMPILER=/usr/local/bin/gfortran-9
         -D__CRMCSTATIC__=ON
       ]
       system "cmake", buildpath, *args
