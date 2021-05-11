cd53d7759ab501799ecfc7837a4e7a86bc9a7b90
chenxu <chenxu@mail.ustc.edu.cn>
2021-05-11 14:55:45 +0800

    erfa 1.7.3

--- Formula/erfa.rb
+++ Formula/erfa.rb
@@ -1,7 +1,7 @@
 class Erfa < Formula
   desc "Essential Routines for Fundamental Astronomy"
   homepage "https://github.com/liberfa/erfa"
-  url "https://github.com/liberfa/erfa/releases/download/v1.7.2/erfa-1.7.2.tar.gz"
+  url "https://github.com/liberfa/erfa/releases/download/v1.7.3/erfa-1.7.3.tar.gz"
   #  sha256 "f0787e30e848750c0cbfc14827de6fc7f69a2d5ef0fc653504e74b8967a764e0"
 
   def install
