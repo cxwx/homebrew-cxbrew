d0e9f7454ca2739e7d5da71bdc66ddfe80d847f5
chenxu <chenxu@mail.ustc.edu.cn>
2021-05-28 16:57:15 +0800

    del gcc@9 upgrade crmc 1.8.0

--- Formula/crmc.rb
+++ Formula/crmc.rb
@@ -1,18 +1,19 @@
 class Crmc < Formula
   desc "Cosmic Ray Monte Carlo"
   homepage "https://devel-ik.fzk.de/wsvn/mc/crmc/"
-  #  url "https://github.com/alisw/crmc/archive/v1.7.0.tar.gz"
-  url "https://gitlab.ikp.kit.edu/AirShowerPhysics/crmc/-/archive/svn/crmc.v1.7.1/crmc-svn-crmc.v1.7.1.tar.gz"
+#  url "https://gitlab.ikp.kit.edu/AirShowerPhysics/crmc/-/archive/svn/crmc.v1.7.1/crmc-svn-crmc.v1.7.1.tar.gz"
+  url "https://gitlab.ikp.kit.edu/AirShowerPhysics/crmc/-/archive/crmc.v1.8.0/crmc-crmc.v1.8.0.tar.gz"
 
   depends_on "cmake" => :build
-  depends_on "gcc@9"
+#  depends_on "gcc@9"
+  depends_on "gcc"
   # depends_on "gcc" # gcc 10 bug
 
   def install
     mkdir "../build" do
       args = %W[
         -DCMAKE_INSTALL_PREFIX=#{prefix}
-        -DCMAKE_Fortran_COMPILER=/usr/local/bin/gfortran-9
+        -DCMAKE_Fortran_COMPILER=/usr/local/bin/gfortran-11
         -D__CRMCSTATIC__=ON
       ]
       system "cmake", buildpath, *args
