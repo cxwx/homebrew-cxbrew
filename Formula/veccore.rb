a0d3173f3171e45dee6520f5624c08a8ad3b2d5e
chenxu <chenxu@mail.ustc.edu.cn>
2021-06-04 16:19:23 +0800

    veccore

--- Formula/veccore.rb
+++ Formula/veccore.rb
@@ -1,10 +1,10 @@
 class Veccore < Formula
   desc "C++ Library for Portable SIMD Vectorization"
   homepage "https://github.com/root-project/veccore"
-  url "https://github.com/root-project/veccore/archive/v0.6.0.tar.gz"
+  url "https://github.com/root-project/veccore/archive/v0.7.0.tar.gz"
 
   depends_on "cmake" => :build
-  depends_on "root"
+#  depends_on "root"
   depends_on "umesimd"
   depends_on "vc"
 
@@ -12,11 +12,11 @@ class Veccore < Formula
     mkdir "builddir" do
       args = std_cmake_args + %w[
         ../
-        -DROOT=ON
         -DBACKEND=Vc
         -DVc_DIR=/usr/local/lib/cmake/Vc/
         -DBUILD_SHARED_LIBS=ON
       ]
+        #-DROOT=ON
       system "cmake", *args
       system "make", "install"
     end
