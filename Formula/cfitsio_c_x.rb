4a30f8024594fc9722e52a5e3b82b338916b395d
chenxu <chenxu@mail.ustc.edu.cn>
2021-06-10 20:09:50 +0800

    for tmp cfitsio

---
+++ Formula/cfitsio_c_x.rb
@@ -0,0 +1,29 @@
+class CfitsioCX < Formula
+  desc "C access to FITS data files with optional Fortran wrappers"
+  homepage "https://heasarc.gsfc.nasa.gov/docs/software/fitsio/fitsio.html"
+  url "https://heasarc.gsfc.nasa.gov/FTP/software/fitsio/c/cfitsio-3.47.tar.gz"
+  version "3.470"
+
+  conflicts_with "cfitsio", because: "both install `cfitsio`"
+
+  livecheck do
+    url :homepage
+    regex(/Download the latest v?(\d+(?:\.\d+)+) version of CFITSIO/i)
+  end
+
+  def install
+    system "./configure", "--prefix=#{prefix}", "--enable-reentrant"
+    system "make", "shared"
+    system "make", "install"
+    (pkgshare/"testprog").install Dir["testprog*"]
+  end
+
+  test do
+    cp Dir["#{pkgshare}/testprog/testprog*"], testpath
+    system ENV.cc, "testprog.c", "-o", "testprog", "-I#{include}",
+                   "-L#{lib}", "-lcfitsio"
+    system "./testprog > testprog.lis"
+    cmp "testprog.lis", "testprog.out"
+    cmp "testprog.fit", "testprog.std"
+  end
+end
