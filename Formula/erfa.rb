d30f2f3dc3a99bd43ad643c5034473143ed9e543
chenxu <chenxu@mail.ustc.edu.cn>
2023-10-16 23:03:30 +0800

    erfa sofa

--- Formula/erfa.rb
+++ Formula/erfa.rb
@@ -1,12 +1,19 @@
 class Erfa < Formula
   desc "Essential Routines for Fundamental Astronomy"
   homepage "https://github.com/liberfa/erfa"
-  url "https://github.com/liberfa/erfa/releases/download/v2.0.0/erfa-2.0.0.tar.gz"
+  #url "https://github.com/liberfa/erfa/releases/download/v2.0.1/erfa-2.0.1.tar.gz"
+  #url "https://codeload.github.com/liberfa/erfa/tar.gz/refs/tags/v2.0.1"
+  url "https://github.com/liberfa/erfa/archive/refs/tags/v2.0.1.tar.gz"
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
 
   def install
-    system "./configure", "--disable-debug",
-                          "--disable-dependency-tracking",
+    system "./bootstrap.sh"
+    system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
+    system "make"
     system "make", "install"
   end
 end
--- Formula/sofa_c.rb
+++ Formula/sofa_c.rb
@@ -1,7 +1,8 @@
 class SofaC < Formula
   desc "Standards of Fundamental Astronomy"
   homepage "http://www.iausofa.org/"
-  url "http://www.iausofa.org/2021_0512_C/sofa_c-20210512.tar.gz"
+#  url "http://www.iausofa.org/2021_0512_C/sofa_c-20210512.tar.gz"
+  url "https://www.iausofa.org/2023_1011_C/sofa_c-20231011.tar.gz"
 #  version "20210125_a"
 
   def install
