4b535d055dc00db5a3defc4cff1c5693795acf97
chenxu <chenxu@mail.ustc.edu.cn>
2024-07-20 00:21:31 +0800

    audit

--- Formula/dealii.rb
+++ Formula/dealii.rb
@@ -1,33 +1,35 @@
 # cspell:disable
 class Dealii < Formula
-  desc "deal.II finite element library."
-  homepage "www.dealii.org"
+  desc "Deal.II finite element library"
+  homepage "https://www.dealii.org"
   url "https://github.com/dealii/dealii/releases/download/v9.5.2/dealii-9.5.2.tar.gz"
+  sha256 "7930e5218a9807d60cc05c300a3b70f36f4af22c3551a2cd1141fbab013bbaf1"
   license "Apache-2.0"
 
+  # The dependency 'sundials' is commented out due to a bug depends_on "sundials"
+
   depends_on "cmake" => :build
-  depends_on "vtk"
-  depends_on "boost"
   depends_on "arpack"
   depends_on "assimp"
-  depends_on "gmsh"
-  depends_on "hdf5"
-  depends_on "gsl"
+  depends_on "boost"
+  depends_on "cgal"
   depends_on "gcc" # fortran
-  depends_on :xcode
+  depends_on "gmsh"
+  depends_on "gsl"
+  depends_on "hdf5"
   depends_on "muparser"
   depends_on "opencascade"
-  # depends_on "sundials" # bug
   depends_on "symengine"
-  depends_on "cgal"
   depends_on "tbb"
+  depends_on "vtk"
+  depends_on :xcode
 
   def install
-    args = %w[
-      -DCMAKE_INSTALL_PREFIX=#{prefix}
-    ]
     system "cmake", ".", "-B", "builddir", "-DCMAKE_INSTALL_PREFIX=#{prefix}"
     system "cmake", "--build", "builddir"
     system "cmake", "--install", "builddir"
+  end
+  test do
+    system "echo", "not finished"
   end
 end
