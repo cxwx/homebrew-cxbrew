36aedcd2eb8d48699238fff071aebd77ea4ed73a
chenxu <chenxu@mail.ustc.edu.cn>
2024-12-15 22:14:37 +0800

    rapidyaml

--- Formula/vecgeom.rb
+++ Formula/vecgeom.rb
@@ -1,20 +1,19 @@
+# cspell:disable
 class Vecgeom < Formula
   desc "Geometry modeller library needed by particle detector simulation"
   homepage "https://gitlab.cern.ch/VecGeom/VecGeom"
-  url "https://gitlab.cern.ch/VecGeom/VecGeom/-/archive/v1.1.18/VecGeom-v1.1.18.tar.gz"
+  url "https://gitlab.cern.ch/VecGeom/VecGeom/-/archive/v01.02.10/VecGeom-v01.02.10.tar.gz"
 
-  depends_on "ccache" => :build
   depends_on "cmake" => :build
-#  depends_on "root"
   depends_on "vc"
+  depends_on "veccore"
+  depends_on "xerces-c"
 
   def install
     mkdir "builddir" do
       args = std_cmake_args + %w[
         ../
-        -DBACKEND=Vc
-        -DNO_SPECIALIZATION=ON
-        -DVc_DIR=/usr/local/lib/cmake/Vc/
+        -DVECGEOM_GDML=ON
       ]
       # -DROOT=ON
       # -DBUILD_SHARED_LIBS=ON
