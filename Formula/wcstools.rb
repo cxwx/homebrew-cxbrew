b7f1189179c75a791ca5ca9b688e5c594216d007
chenxu <chenxu@mail.ustc.edu.cn>
2024-12-23 19:02:45 +0800

    wcstools 3.9.7

--- Formula/wcstools.rb
+++ Formula/wcstools.rb
@@ -1,7 +1,12 @@
-class Wcstools20201101 < Formula
+class Wcstools < Formula
   desc "Tools for using World Coordinate Systems (WCS) in astronomical images"
   homepage "http://tdc-www.harvard.edu/wcstools/"
-  url "http://tdc-www.harvard.edu/software/wcstools/wcstools-3.9.6.tar.gz"
+  url "http://tdc-www.harvard.edu/software/wcstools/wcstools-3.9.7.tar.gz"
+  sha256 "f155d2f67bc2844d2d2e0f7082439cdf727dfb98369846d7f78febf7a270c8a6"
+  license "LGPL-2.0-only"
+
+  depends_on :macos
+
   def install
     system "make", "-f", "Makefile.osx", "all"
 
