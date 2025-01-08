d2e1065b3151f9d34c0f7fed5c94702c2ffbc88e
chenxu <chenxu@mail.ustc.edu.cn>
2025-01-08 15:29:00 +0800

    ftxui -> head

--- Formula/ftxui.rb
+++ Formula/ftxui.rb
@@ -15,25 +15,23 @@ class Ftxui < Formula
   desc "C++ Functional Terminal User Interface."
   homepage "https://github.com/ArthurSonzogni/FTXUI"
   url "https://github.com/ArthurSonzogni/FTXUI/archive/refs/tags/v5.0.0.tar.gz"
-  head "https://github.com/ArthurSonzogni/FTXUI", branch: "main"
+  head "https://github.com/ArthurSonzogni/FTXUI.git", branch: "main"
 
   depends_on "cmake" => :build
 
   def install
-    mkdir "geant-build" do
-      args = std_cmake_args + %w[
-        ../
-        -DBUILD_SHARED_LIBS=ON
-        -DFTXUI_BUILD_DOCS=ON
-        -DFTXUI_BUILD_EXAMPLES=ON
-        -DFTXUI_BUILD_TESTS=ON
-        -DFTXUI_QUIET=ON
-        -DFTXUI_ENABLE_COVERAGE=ON
-      ]
+    args = std_cmake_args + %w[
+      -DBUILD_SHARED_LIBS=ON
+      -DFTXUI_BUILD_DOCS=ON
+      -DFTXUI_BUILD_EXAMPLES=ON
+      -DFTXUI_BUILD_TESTS=ON
+      -DFTXUI_QUIET=ON
+      -DFTXUI_ENABLE_COVERAGE=ON
+    ]
 
-      system "cmake", *args
-      system "make", "install"
-    end
+    system "cmake", "-S" ,".", "-B", "builddir", *args
+    system "cmake", "--build", "builddir"
+    system "cmake", "--install", "builddir"
   end
 
 end
