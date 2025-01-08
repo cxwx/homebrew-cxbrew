db8bb66ca5bc966fd7a85835dd8614c4b13df7aa
chenxu <chenxu@mail.ustc.edu.cn>
2025-01-08 16:43:46 +0800

    reflect-cpp

---
+++ Formula/reflect-cpp.rb
@@ -0,0 +1,43 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    ftxui.rb                                           :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: chenxu <chenxu@mail.ustc.edu.cn>           +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2025/01/08 01:41:10 by chenxu            #+#    #+#              #
+#    Updated: 2025/01/08 01:43:39 by chenxu           ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+# cspell:disable
+class ReflectCpp < Formula
+  desc "A C++20 library for fast serialization, deserialization and validation using reflection."
+  homepage "https://github.com/getml/reflect-cpp"
+  url "https://github.com/getml/reflect-cpp/archive/refs/tags/v0.17.0.tar.gz"
+  sha256 "08b6406cbe4c6c14ff1a619fe93a94f92f6d9eb22213d93529ad975993945e45"
+  head "https://github.com/getml/reflect-cpp.git", branch: "main"
+
+  depends_on "cmake" => :build
+  depends_on "make" => :build
+  # depends_on "vcpkg" => :build
+  depends_on "tomlplusplus"
+  depends_on "yaml-cpp"
+  depends_on "pugixml"
+
+  def install
+    args = std_cmake_args + %w[
+      -DCMAKE_MAKE_PROGRAM=gmake
+      -DREFLECTCPP_JSON=ON
+      -DREFLECTCPP_YAML=ON
+      -DREFLECTCPP_USE_VCPKG=OFF
+      -DREFLECTCPP_TOML=ON
+      -DREFLECTCPP_XML=ON
+    ]
+
+    system "cmake", "-S" ,".", "-B", "builddir", *args
+    system "cmake", "--build", "builddir"
+    system "cmake", "--install", "builddir"
+  end
+
+end
