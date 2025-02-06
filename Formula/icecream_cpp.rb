8f436fd74ac19cb395be68b566611acda729292b
chenxu <chenxu@mail.ustc.edu.cn>
2025-02-06 22:03:43 +0800

    icecream-cpp

--- Formula/icecream_cpp.rb
+++ Formula/icecream_cpp.rb
@@ -14,17 +14,16 @@
 class IcecreamCpp < Formula
   desc "Never use cout/printf to debug again"
   homepage "https://github.com/renatoGarcia/icecream-cpp"
+  url "https://github.com/renatoGarcia/icecream-cpp"
   head "https://github.com/renatoGarcia/icecream-cpp.git", branch: "master"
 
   depends_on "cmake" => :build
 
   def install
-    args = std_cmake_args + %w[
-    ]
+    args = std_cmake_args + %w[]
 
-    system "cmake", "-S" ,".", "-B", "builddir", *args
+    system "cmake", "-S", ".", "-B", "builddir", *args
     system "cmake", "--build", "builddir"
     system "cmake", "--install", "builddir"
   end
-
 end
