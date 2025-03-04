cd15efb9fb5e61c55d8899b4ff85f42524109ea9
chenxu <6235641+cxwx@users.noreply.github.com>
2025-03-04 18:25:51 +0800

    mv pal to cmake

--- Formula/pal.rb
+++ Formula/pal.rb
@@ -1,27 +1,34 @@
 class Pal < Formula
   homepage "https://github.com/IceCube-SPNO/pal"
-  url "https://github.com/Starlink/pal/releases/download/v0.9.8/pal-0.9.8.tar.gz"
+  # url "https://github.com/Starlink/pal/releases/download/v0.9.8/pal-0.9.8.tar.gz"
 #  url "https://github.com/Starlink/pal/releases/download/v0.9.10/pal-0.9.10.tar.gz"
 #  sha256 "191fde8c4f45d6807d4b011511344014966bb46e44029a4481d070cd5e7cc697"
-#  head "https://github.com/Starlink/pal", using: :git  
+  head "https://github.com/cxwx/pal.git", using: :git, branch: "master"
 #  version "0.9.10"
 
   depends_on "erfa"
-  # batteries not included
-  if RUBY_PLATFORM.match?(/darwin/)
-    depends_on "autoconf" => :build
-    depends_on "automake" => :build
-    depends_on "libtool"  => :build
-  end
+  # if RUBY_PLATFORM.match?(/darwin/)
+  #   depends_on "autoconf" => :build
+  #   depends_on "automake" => :build
+  #   depends_on "libtool"  => :build
+  # end
+  depends_on "cmake" => :build
 
+  # def install
+  #   #    system "autoreconf", "--install", "--symlink", buildpath
+  #   # system "autoreconf" "--fiv"
+  #   system "./configure", "--disable-debug",
+  #                         "--without-starlink",
+  #                         #                          "--disable-dependency-tracking",
+  #                         #                          "--disable-silent-rules",
+  #                         "--prefix=#{prefix}"
+  #   system "make", "install"
+  # end
   def install
-    #    system "autoreconf", "--install", "--symlink", buildpath
-    # system "autoreconf" "--fiv"
-    system "./configure", "--disable-debug",
-                          "--without-starlink",
-                          #                          "--disable-dependency-tracking",
-                          #                          "--disable-silent-rules",
-                          "--prefix=#{prefix}"
-    system "make", "install"
+    args = %w[]
+
+    system "cmake", "-S", ".", "-B", "builddir", *args, *std_cmake_args
+    system "cmake", "--build", "builddir"
+    system "cmake", "--install", "builddir"
   end
 end
