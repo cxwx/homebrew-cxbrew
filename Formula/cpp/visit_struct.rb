941ad2c1310175684b6c83498da3e07de5aa6c6f
chenxu <6235641+cxwx@users.noreply.github.com>
2025-03-06 03:24:03 +0800

    visit_struct

---
+++ Formula/cpp/visit_struct.rb
@@ -0,0 +1,23 @@
+class VisitStruct < Formula
+  desc "miniature library for struct-field reflection in C++"
+  homepage "https://github.com/cxwx/visit_struct"
+  license "Boost Software License"
+  head "https://github.com/cxwx/visit_struct.git", branch: "master"
+
+  depends_on "cmake" => :build
+
+  def install
+    args = %w[]
+
+    system "cmake", "-S", ".", "-B", "builddir", *args, *std_cmake_args
+    system "cmake", "--build", "builddir"
+    system "cmake", "--install", "builddir"
+  end
+
+  test do
+    (testpath/"test.cpp").write <<~CPP
+    CPP
+    system ENV.cxx, "test.cpp", "-std=c++11", "-o", "test"
+    system "./test"
+  end
+end
