ee4ffb0e06c961f8be1df928f208e80ed21749fa
chenxu <6235641+cxwx@users.noreply.github.com>
2025-03-06 13:12:20 +0800

    async_simple for core

--- Formula/async_simple.rb
+++ Formula/async_simple.rb
@@ -1,26 +1,15 @@
-# **************************************************************************** #
-#                                                                              #
-#                                                         :::      ::::::::    #
-#    createmodel_c_x.rb                                 :+:      :+:    :+:    #
-#                                                     +:+ +:+         +:+      #
-#    By: chenxu <chenxu@mail.ustc.edu.cn>           +#+  +:+       +#+         #
-#                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2025/01/09 19:29:29 by chenxu            #+#    #+#              #
-#    Updated: 2025/01/09 19:29:30 by chenxu           ###   ########.fr        #
-#                                                                              #
-# **************************************************************************** #
-
-# cspell:disable
 class AsyncSimple < Formula
-  desc "https://github.com/alibaba/async_simple"
+  desc "Simple, light-weight and easy-to-use asynchronous components"
   homepage "https://github.com/alibaba/async_simple"
   url "https://github.com/alibaba/async_simple/archive/refs/tags/v1.3.tar.gz"
+  sha256 "0ba0dc3397882611b538d04b8ee6668b1a04ce046128599205184c598b718743"
+  license "Apache-2.0"
   head "https://github.com/alibaba/async_simple.git", branch: "main"
 
   depends_on "cmake" => :build
 
   def install
-    args = std_cmake_args + %w[
+    args = %w[
       -DCMAKE_CXX_STANDARD=20
       -DBUILD_GMOCK=OFF
       -DBUILD_GTEST=OFF
@@ -29,9 +18,35 @@ class AsyncSimple < Formula
       -DASYNC_SIMPLE_ENABLE_TESTS=OFF
     ]
 
-    system "cmake", "-S" ,".", "-B", "builddir", *args
+    system "cmake", "-S", ".", "-B", "builddir", *args, *std_cmake_args
     system "cmake", "--build", "builddir"
     system "cmake", "--install", "builddir"
   end
+  test do
+    (testpath / "test.cpp").write <<~CPP
+      #include <algorithm>
+      #include <async_simple/coro/Lazy.h>
+      #include <async_simple/coro/SyncAwait.h>
+      #include <iostream>
+      #include <string>
+      #include <vector>
 
+      using namespace async_simple::coro;
+
+      using Texts = std::vector<std::string>;
+
+      Lazy<int> CountLineChar(const std::string& line, char c) {
+        auto ret = std::count(line.begin(), line.end(), c);
+        co_return static_cast<int>(ret);
+      }
+
+      auto main() -> int {
+        auto num = syncAwait(CountLineChar("axxxax", 'x'));
+        std::cout << num << std::endl;
+      }
+    CPP
+    ENV.append "LDFLAGS", "-L/opt/homebrew/lib"
+    system ENV.cxx, "test.cpp", "-std=c++20", "-L#{lib}", "-lasync_simple", "-o", "test"
+    assert_equal "4\n", shell_output("./test")
+  end
 end
