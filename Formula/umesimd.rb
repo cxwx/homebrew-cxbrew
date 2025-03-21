dad1bc204e96aa2e93e42c15dc33875b0daa4847
chenxu <6235641+cxwx@users.noreply.github.com>
2025-03-21 23:56:49 +0800

    umesimd

--- Formula/umesimd.rb
+++ Formula/umesimd.rb
@@ -1,19 +1,33 @@
-# cspell:disable
 class Umesimd < Formula
   desc "Explicit vectorization library"
   homepage "https://github.com/edanor/umesimd"
-  url "https://github.com/edanor/umesimd/archive/v0.8.1.tar.gz"
+  # url "https://github.com/edanor/umesimd/archive/v0.8.1.tar.gz"
+  head "https://github.com/edanor/umesimd.git", branch: "master"
+  license "MIT"
 
   depends_on "cmake" => :build
 
   def install
-    mkdir "builddir" do
-      args = std_cmake_args + %w[
-        ../
-        -DBUILD_SHARED_LIBS=ON
-      ]
-      system "cmake", *args
-      system "make", "install"
-    end
+    system "cmake", "-S", ".", "-B", "builddir", *std_cmake_args
+    system "cmake", "--build", "builddir"
+    system "cmake", "--install", "builddir"
+  end
+
+  test do
+    (testpath/"test.cpp").write <<~CPP
+      #include <umesimd/UMESimd.h>
+      template<typename VEC_T>
+        void printVector(VEC_T & v) {
+        for(int i = 0; i < VEC_T::length(); i++) {
+          std::cout << v[i] << " ";
+        }
+      }
+      int main() {
+        UME::SIMD::SIMD4_64f a(5., 3., 8., 4.);
+        printVector(a);
+      }
+    CPP
+    system ENV.cxx, "test.cpp", "-std=c++11", "-o", "test"
+    assert_equal "5 3 8 4 ", shell_output("./test").chomp
   end
 end
