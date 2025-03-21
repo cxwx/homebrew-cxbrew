1947efdadd9d0fe0101a6534ca75ea2198c7c19b
chenxu <6235641+cxwx@users.noreply.github.com>
2025-03-21 18:53:24 +0800

    merge to core

--- Formula.old/cpp-lazy.rb
+++ Formula.old/cpp-lazy.rb
@@ -1,16 +1,17 @@
 class CppLazy < Formula
   desc "C++11 (and onwards) library for lazy evaluation"
   homepage "https://github.com/Kaaserne/cpp-lazy"
-  url "https://github.com/Kaaserne/cpp-lazy/archive/refs/tags/v8.0.0.tar.gz"
-  sha256 "02f29ff1e6b44fc0e34cb7c651ee4b4f806c63dfc699fc135db794424fdc119f"
+  url "https://github.com/Kaaserne/cpp-lazy/archive/refs/tags/v8.0.1.tar.gz"
+  sha256 "b237a7df04006f62c19eedaf7adbf9eb8d4dd4fd3daee39ca13d3aac97254f90"
   license "MIT"
   head "https://github.com/Kaaserne/cpp-lazy.git", branch: "master"
 
   depends_on "cmake" => :build
   depends_on "fmt" => :test
+  depends_on "pkgconf" => :test
 
   def install
-    args = std_cmake_args + %w[
+    args = %w[
       -DCPP-LAZY_USE_STANDALONE=ON
     ]
 
@@ -21,18 +22,17 @@ class CppLazy < Formula
 
   test do
     (testpath/"test.cpp").write <<~CPP
-      #include <cpp-lazy-7.0.2/Lz/Map.hpp>
+      #include <cpp-lazy/Lz/Map.hpp>
 
       int main() {
         std::array<int, 4> arr = {1, 2, 3, 4};
         std::string result = lz::map(arr, [](int i) { return i + 1; }).toString(" "); // == "1 2 3 4"
       }
     CPP
-    fmt_prefix = Formula["fmt"].opt_prefix
-    fmt_include = fmt_prefix/"include"
-    fmt_lib = fmt_prefix/"lib"
-
-    system ENV.cxx, "test.cpp", "-std=c++11", "-o", "test", "-I#{fmt_include}/fmt", "-L#{fmt_lib}", "-lfmt"
+    ENV.prepend_path "PKG_CONFIG_PATH", Formula["fmt"].opt_lib/"pkgconfig"
+    cxxflags = shell_output("pkg-config --cflags fmt").strip
+    ldflags = shell_output("pkg-config --libs fmt").strip
+    system ENV.cxx, "test.cpp", "-std=c++11", "-o", "test", *cxxflags.split, *ldflags.split
     system "./test"
   end
 end
