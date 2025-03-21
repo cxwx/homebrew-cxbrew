611ea769f7f30ee3e44cb6242caf553a9079eee3
chenxu <6235641+cxwx@users.noreply.github.com>
2025-03-21 23:57:04 +0800

    veccore

--- Formula/veccore.rb
+++ Formula/veccore.rb
@@ -1,23 +1,34 @@
-# cspell:disable
 class Veccore < Formula
   desc "C++ Library for Portable SIMD Vectorization"
   homepage "https://github.com/root-project/veccore"
-  url "https://github.com/root-project/veccore/archive/v0.8.2.tar.gz"
+  url "https://github.com/root-project/veccore/archive/refs/tags/v0.8.2.tar.gz"
+  sha256 "1268bca92acf00acd9775f1e79a2da7b1d902733d17e283e0dd5e02c41ac9666"
+  head "https://github.com/root-project/veccore.git"
 
   depends_on "cmake" => :build
-  depends_on "umesimd"
+  # depends_on "umesimd" -- old
   depends_on "vc"
 
   def install
-    mkdir "builddir" do
-      args = std_cmake_args + %w[
-        ../
-        -DUMESIMD=OFF
-        -DVC=ON
-        -DBUILD_SHARED_LIBS=ON
-      ]
-      system "cmake", *args
-      system "make", "install"
-    end
+    args = %w[
+      -DUMESIMD=OFF
+      -DVC=ON
+    ]
+
+    system "cmake", "-S", ".", "-B", "builddir", *args, *std_cmake_args
+    system "cmake", "--build", "builddir"
+    system "cmake", "--install", "builddir"
+  end
+
+  test do
+    (testpath/"test.cpp").write <<~CPP
+      #include <VecCore/VecCore>
+      #include <iostream>
+      auto main() -> int {
+        std::cout<<vecCore::Rad<double>(0.0)<<std::endl;
+      }
+    CPP
+    system ENV.cxx, "test.cpp", "-std=c++20", "-o", "test"
+    assert_equal "0", shell_output("./test").chomp
   end
 end
