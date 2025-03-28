a52272f5af2ce2df7a96f85ae734f691e45a2020
chenxu <6235641+cxwx@users.noreply.github.com>
2025-03-28 09:21:45 +0800

    vdt linux

--- Formula/vdt.rb
+++ Formula/vdt.rb
@@ -1,13 +1,15 @@
 # TODO: SSE BUG
 class Vdt < Formula
-  desc "vdt Mathematical library"
+  desc "C++ math library of fast approximate and vectorisable trascendental functions"
   homepage "https://github.com/dpiparo/vdt"
   url "https://github.com/dpiparo/vdt/archive/refs/tags/v0.4.4.tar.gz"
   sha256 "8b1664b45ec82042152f89d171dd962aea9bb35ac53c8eebb35df1cb9c34e498"
-  license "LGPL"
+  license "LGPL-3.0-or-later"
   head "https://github.com/dpiparo/vdt.git", branch: "master"
 
   depends_on "cmake" => :build
+
+  uses_from_macos "python" => :build
 
   def install
     system "cmake", "-S", ".", "-B", "builddir", *std_cmake_args
