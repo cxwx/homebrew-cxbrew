34674c96ea08575f0a5b0e5af0938d4a4f99fe49
chenxu <6235641+cxwx@users.noreply.github.com>
2025-03-31 16:15:12 +0800

    lazycsv

---
+++ Formula/cpp/lazycsv.rb
@@ -0,0 +1,36 @@
+class Lazycsv < Formula
+  desc "Fast, lightweight and single-header C++ csv parser library"
+  homepage "https://github.com/ashtum/lazycsv"
+  license "MIT"
+  head "https://github.com/ashtum/lazycsv.git", branch: "master"
+
+  depends_on "cmake" => :build
+
+  # TODO: github:ashtum/lazycsv/pull/14
+  def install
+    args = %w[
+      -DBUILD_SHARED_LIBS=ON
+      -DCMAKE_CXX_STANDARD=17
+    ]
+
+    system "cmake", "-S", ".", "-B", "builddir", *args, *std_cmake_args
+    system "cmake", "--build", "builddir"
+    system "cmake", "--install", "builddir"
+  end
+
+  test do
+    (testpath/"test.cpp").write <<~CPP
+      #include <cpp-lazy/Lz/Map.hpp>
+
+      int main() {
+        std::array<int, 4> arr = {1, 2, 3, 4};
+        std::string result = lz::map(arr, [](int i) { return i + 1; }).toString(" "); // == "1 2 3 4"
+      }
+    CPP
+    ENV.prepend_path "PKG_CONFIG_PATH", Formula["fmt"].opt_lib/"pkgconfig"
+    cxxflags = shell_output("pkg-config --cflags fmt").strip
+    ldflags = shell_output("pkg-config --libs fmt").strip
+    system ENV.cxx, "test.cpp", "-std=c++17", "-o", "test", *cxxflags.split, *ldflags.split
+    system "./test"
+  end
+end
