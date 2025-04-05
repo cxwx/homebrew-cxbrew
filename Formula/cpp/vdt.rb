e93712be5c7de3629dc78df94dc930bfc5498de2
chenxu <6235641+cxwx@users.noreply.github.com>
2025-04-05 13:15:45 +0800

    vdt

--- Formula/cpp/vdt.rb
+++ Formula/cpp/vdt.rb
@@ -2,8 +2,8 @@
 class Vdt < Formula
   desc "C++ math library of fast approximate and vectorisable trascendental functions"
   homepage "https://github.com/dpiparo/vdt"
-  url "https://github.com/dpiparo/vdt/archive/refs/tags/v0.4.4.tar.gz"
-  sha256 "8b1664b45ec82042152f89d171dd962aea9bb35ac53c8eebb35df1cb9c34e498"
+  url "https://github.com/dpiparo/vdt/archive/refs/tags/v0.4.6.tar.gz"
+  sha256 "1820feae446780763ec8bbb60a0dbcf3ae1ee548bdd01415b1fb905fd4f90c54"
   license "LGPL-3.0-or-later"
   head "https://github.com/dpiparo/vdt.git", branch: "master"
 
@@ -12,19 +12,22 @@ class Vdt < Formula
   uses_from_macos "python" => :build
 
   def install
-    system "cmake", "-S", ".", "-B", "builddir", *std_cmake_args
+    system "cmake", "-S", ".", "-B", "builddir",
+      "-DCMAKE_POLICY_VERSION_MINIMUM=3.5",
+      "-DSSE=OFF",
+      *std_cmake_args
     system "cmake", "--build", "builddir"
     system "cmake", "--install", "builddir"
   end
 
   test do
     (testpath/"test.cpp").write <<~CPP
-      #include <vdt/sin.h>
-      #include <iostream>
-      using namespace std;
-      int main() {
-        cout << vdt::fast_sin(0.0) << endl;
-      }
+    #include <vdt/sin.h>
+    #include <iostream>
+    using namespace std;
+    int main() {
+      cout << vdt::fast_sin(0.0) << endl;
+    }
     CPP
     system ENV.cxx, "test.cpp", "-std=c++14", "-o", "test", "-L#{lib}", "-lvdt"
     assert_equal "0", shell_output("./test").chomp
