e350686ddbcca1a70918a22320a814d9d12b097a
chenxu <6235641+cxwx@users.noreply.github.com>
2025-08-27 00:09:03 +0800

    meta-morph(new Formula)
    head only

---
+++ Formula/cpp/meta-morph.rb
@@ -0,0 +1,30 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    ftxui.rb                                           :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: chenxu <chenxu@mail.ustc.edu.cn>           +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2025/01/08 01:41:10 by chenxu            #+#    #+#              #
+#    Updated: 2025/01/08 01:43:39 by chenxu           ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+# cspell:disable
+class MetaMorph < Formula
+  desc "A small C++ Decorator Library"
+  homepage "https://github.com/DeForestt/meta-morph"
+  head "https://github.com/DeForestt/meta-morph.git", branch: "main"
+
+  depends_on "cmake" => :build
+
+  def install
+    args = std_cmake_args + %w[
+    ]
+
+    system "cmake", "-S" ,".", "-B", "builddir", *args
+    system "cmake", "--build", "builddir"
+    system "cmake", "--install", "builddir"
+  end
+
+end
