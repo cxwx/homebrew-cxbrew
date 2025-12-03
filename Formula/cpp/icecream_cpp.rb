3a0c862ae1e103036b5da31b1f871f49de40ad73
chenxu <6235641+cxwx@users.noreply.github.com>
2025-12-03 11:17:59 +0800

    icecream_cpp

--- Formula/cpp/icecream_cpp.rb
+++ Formula/cpp/icecream_cpp.rb
@@ -1,7 +1,7 @@
 class IcecreamCpp < Formula
   desc "Never use cout/printf to debug again"
   homepage "https://github.com/renatoGarcia/icecream-cpp"
-  url "https://github.com/renatoGarcia/icecream-cpp"
+  url "https://github.com/renatoGarcia/icecream-cpp/archive/refs/tags/v1.0.0.tar.gz"
   head "https://github.com/renatoGarcia/icecream-cpp.git", branch: "master"
 
   depends_on "cmake" => :build
