249ceb2050a83252ca71e4e783ae074bd8b46c6f
chenxu <6235641+cxwx@users.noreply.github.com>
2025-12-31 15:16:38 +0800

    stltostp 1.0.2

---
+++ Formula/cpp/stltostp.rb
@@ -0,0 +1,33 @@
+class Stltostp < Formula
+  desc "Convert stl files to STEP brep files"
+  homepage "https://github.com/slugdev/stltostp"
+  # url "https://github.com/louis-langholtz/PlayRho/archive/refs/tags/v1.1.2.tar.gz"
+  # sha256 "b8e61eace48607c545a495c742479fb58e7ea34b8dfe7f874a14b8414135cccc"
+  license "BSD-4-Clause"
+  head "https://github.com/slugdev/stltostp.git", branch: "master"
+
+  depends_on "cmake" => :build
+
+  def install
+    args = %w[]
+
+    system "cmake", "-S", ".", "-B", "builddir", *args, *std_cmake_args
+    system "cmake", "--build", "builddir"
+    system "cmake", "--install", "builddir"
+  end
+
+  # test do
+  #   (testpath/"test.cpp").write <<~CPP
+  #     #include <cpp-lazy/Lz/Map.hpp>
+  #     int main() {
+  #       std::array<int, 4> arr = {1, 2, 3, 4};
+  #       std::string result = lz::map(arr, [](int i) { return i + 1; }).toString(" "); // == "1 2 3 4"
+  #     }
+  #   CPP
+  #   ENV.prepend_path "PKG_CONFIG_PATH", Formula["fmt"].opt_lib/"pkgconfig"
+  #   cxxflags = shell_output("pkg-config --cflags fmt").strip
+  #   ldflags = shell_output("pkg-config --libs fmt").strip
+  #   system ENV.cxx, "test.cpp", "-std=c++11", "-o", "test", *cxxflags.split, *ldflags.split
+  #   system "./test"
+  # end
+end
