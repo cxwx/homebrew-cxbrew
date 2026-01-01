aa0bda2b1ef7438f88a150ffe895f5964466acd1
chenxu <6235641+cxwx@users.noreply.github.com>
2026-01-01 20:44:34 +0800

    reflect-cpp upgrade

--- Formula/cpp/reflect-cpp.rb
+++ Formula/cpp/reflect-cpp.rb
@@ -1,27 +1,14 @@
-# **************************************************************************** #
-#                                                                              #
-#                                                         :::      ::::::::    #
-#    ftxui.rb                                           :+:      :+:    :+:    #
-#                                                     +:+ +:+         +:+      #
-#    By: chenxu <chenxu@mail.ustc.edu.cn>           +#+  +:+       +#+         #
-#                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2025/01/08 01:41:10 by chenxu            #+#    #+#              #
-#    Updated: 2025/01/08 01:43:39 by chenxu           ###   ########.fr        #
-#                                                                              #
-# **************************************************************************** #
-
-# cspell:disable
 class ReflectCpp < Formula
   desc "A C++20 library for fast serialization, deserialization and validation using reflection."
   homepage "https://github.com/getml/reflect-cpp"
-  url "https://github.com/getml/reflect-cpp/archive/refs/tags/v0.18.0.tar.gz"
+  url "https://github.com/getml/reflect-cpp/archive/refs/tags/v0.23.0.tar.gz"
   # sha256 "08b6406cbe4c6c14ff1a619fe93a94f92f6d9eb22213d93529ad975993945e45"
   head "https://github.com/getml/reflect-cpp.git", branch: "main"
 
   depends_on "cmake" => :build
   depends_on "make" => :build
   # depends_on "vcpkg" => :build
-  # TODO: add support: https://github.com/getml/reflect-cpp?tab=readme-ov-file#serialization-formats
+  # TODO: add support:
   depends_on "tomlplusplus"
   depends_on "toml11"
   depends_on "yaml-cpp"
