afbdfc7cdf72192c01afed826d6f6ce933c62188
chenxu <6235641+cxwx@users.noreply.github.com>
2026-01-09 11:01:25 +0800

    bilibili-tui(new Formula)
    cc

---
+++ Formula/cargo/bilibili-tui.rb
@@ -0,0 +1,17 @@
+class BilibiliTui < Formula
+  desc "A terminal user interface (TUI) client for Bilibili"
+  homepage "https://maredevi.moe/projects/bilibili-tui/"
+  url "https://github.com/MareDevi/bilibili-tui/archive/refs/tags/v1.0.3.tar.gz"
+  license "MIT"
+  head "https://github.com/maredevi/bilibili-tui.git", branch: "main"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", *std_cargo_args
+  end
+
+  test do
+    # TODO:
+  end
+end
