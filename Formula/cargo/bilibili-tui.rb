ef0a1020fad9ad02a7a4076c43275f5dd318a6e1
chenxu <6235641+cxwx@users.noreply.github.com>
2026-01-10 10:23:34 +0800

    bilibili-tui update 1.0.7

--- Formula/cargo/bilibili-tui.rb
+++ Formula/cargo/bilibili-tui.rb
@@ -1,7 +1,7 @@
 class BilibiliTui < Formula
   desc "A terminal user interface (TUI) client for Bilibili"
   homepage "https://maredevi.moe/projects/bilibili-tui/"
-  url "https://github.com/MareDevi/bilibili-tui/archive/refs/tags/v1.0.3.tar.gz"
+  url "https://github.com/MareDevi/bilibili-tui/archive/refs/tags/v1.0.7.tar.gz"
   license "MIT"
   head "https://github.com/maredevi/bilibili-tui.git", branch: "main"
 
