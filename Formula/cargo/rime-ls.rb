6c0b4d5fdfbc0c2f94c0d51975d9135fe0115e92
chenxu <6235641+cxwx@users.noreply.github.com>
2026-03-15 20:23:04 +0800

    rime-ls (new)

---
+++ Formula/cargo/rime-ls.rb
@@ -0,0 +1,19 @@
+class RimeLs < Formula
+  desc "A language server that provides input method functionality using librime"
+  homepage "https://github.com/wlh320/rime-ls" 
+  # url "https://github.com/wlh320/rime-ls/archive/refs/tags/v0.4.3.tar.gz"
+  # TODO:此版本我记得有内存泄露的问题
+  license "MIT"
+  head "https://github.com/wlh320/rime-ls.git", branch: "master"
+
+  depends_on "rust" => :build
+  depends_on "librime"
+
+  def install
+    system "cargo", "install", *std_cargo_args
+  end
+
+  test do
+    # TODO:
+  end
+end
