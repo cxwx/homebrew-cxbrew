63ac43c291889de2e84732400321b6a75ee80d88
cx <6235641+cxwx@users.noreply.github.com>
2026-05-04 23:53:53 +0800

    reflect-cpp 0.24.0

--- Formula/cpp/reflect-cpp.rb
+++ Formula/cpp/reflect-cpp.rb
@@ -1,33 +1,30 @@
 class ReflectCpp < Formula
-  desc "A C++20 library for fast serialization, deserialization and validation using reflection."
+  desc "C++20 lib for fast serialization deserialization validation using reflection"
   homepage "https://github.com/getml/reflect-cpp"
-  url "https://github.com/getml/reflect-cpp/archive/refs/tags/v0.23.0.tar.gz"
-  sha256 "9c5650d7ef0ab2b0ff617095280c641e6d770d9efc62dc04d86bb6ea56b55130"
+  url "https://github.com/getml/reflect-cpp/archive/refs/tags/v0.24.0.tar.gz"
+  sha256 "2185d45cca58f60bcdaca37a995c6f8e90c0105f312610333fed75f2efa6a996"
   # sha256 "08b6406cbe4c6c14ff1a619fe93a94f92f6d9eb22213d93529ad975993945e45"
   head "https://github.com/getml/reflect-cpp.git", branch: "main"
 
   depends_on "cmake" => :build
   depends_on "make" => :build
-  # depends_on "vcpkg" => :build
   # TODO: add support: from vcpkg
-  depends_on "tomlplusplus"
-  depends_on "toml11"
-  depends_on "yaml-cpp"
-  depends_on "rapidjson"
-  depends_on "nlohmann-json"
-  depends_on "msgpack-c"
-  depends_on "flatbuffers"
-  depends_on "ctre"
-  depends_on "capnp" # https://capnproto.org/
-  depends_on "mongo-c-driver"  # bson
-  depends_on "simdjson"
-  # depends_on "gtest"
-  # depends_on "benchmark"
   depends_on "apache-arrow"
-  depends_on "jsoncons"  # cxwx
-  depends_on "pugixml"
-  depends_on "yyjson" # ?
   depends_on "avro-c"
+  depends_on "capnp"
+  depends_on "ctre"
+  depends_on "flatbuffers"
+  depends_on "jsoncons"
+  depends_on "mongo-c-driver"
+  depends_on "msgpack-c" # https://capnproto.org/ # bson
+  depends_on "nlohmann-json"
+  depends_on "pugixml"
+  depends_on "rapidjson"
+  depends_on "simdjson" # cxwx
+  depends_on "toml11"
+  depends_on "tomlplusplus"
+  depends_on "yaml-cpp"
+  depends_on "yyjson" # ?
 
   # BUG: janssonConfig.cmake not installed, avro=off
   # BUG: arrow::arrow  but the target was not found csv=off
@@ -52,9 +49,11 @@ class ReflectCpp < Formula
       -DREFLECTCPP_PARQUET=OFF
     ]
 
-    system "cmake", "-S" ,".", "-B", "builddir", *args
+    system "cmake", "-S", ".", "-B", "builddir", *args
     system "cmake", "--build", "builddir"
     system "cmake", "--install", "builddir"
   end
-
+  test do
+    echo "hello"
+  end
 end
