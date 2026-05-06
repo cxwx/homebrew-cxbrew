8fb643c33043166ea00425fe7f7bc10406ef870a
cx <6235641+cxwx@users.noreply.github.com>
2026-05-06 19:34:11 +0800

    crmc new version

--- Formula/crmc.rb
+++ Formula/crmc.rb
@@ -1,9 +1,10 @@
 class Crmc < Formula
   desc "Cosmic Ray Monte Carlo"
   homepage "https://devel-ik.fzk.de/wsvn/mc/crmc/"
-#  url "https://gitlab.ikp.kit.edu/AirShowerPhysics/crmc/-/archive/svn/crmc.v1.7.1/crmc-svn-crmc.v1.7.1.tar.gz"
+  # url "https://gitlab.ikp.kit.edu/AirShowerPhysics/crmc/-/archive/svn/crmc.v1.7.1/crmc-svn-crmc.v1.7.1.tar.gz"
   # url "https://gitlab.ikp.kit.edu/AirShowerPhysics/crmc/-/archive/crmc.v1.8.0/crmc-crmc.v1.8.0.tar.gz"
-  url "https://gitlab.iap.kit.edu/AirShowerPhysics/crmc/-/archive/v2.2.1/crmc-v2.2.1.tar.gz"
+  # url "https://gitlab.iap.kit.edu/AirShowerPhysics/crmc/-/archive/v2.2.1/crmc-v2.2.1.tar.gz"
+  url "https://gitlab.iap.kit.edu/AirShowerPhysics/crmc/-/archive/v2.2.1_QGSJET_EXTEND_MASS_RANGE_v0/crmc-v2.2.1_QGSJET_EXTEND_MASS_RANGE_v0.tar.gz"
 
   depends_on "cmake" => :build
   depends_on "gcc"
