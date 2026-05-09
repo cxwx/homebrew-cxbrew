f8b2cf0e4a000c4f3fd08251ea723612ad4e6f66
cx <6235641+cxwx@users.noreply.github.com>
2026-05-09 22:23:13 +0800

    feat(formula): add mathplot formula for C++ plotting

---
+++ Formula/cpp/mathplot.rb
@@ -0,0 +1,24 @@
+class Mathplot < Formula
+  desc "Plotting, graphing and data visualization for C++ programs"
+  homepage "https://github.com/sebsjames/mathplot"
+  head "https://github.com/sebsjames/mathplot.git", branch: "main", submodules: true
+
+  depends_on "cmake" => :build
+  depends_on "freetype"
+  depends_on "glfw"
+  depends_on "nlohmann-json"
+
+  def install
+    system "cmake", "-S", ".", "-B", "build",
+           "-DBUILD_EXAMPLES=OFF",
+           "-DBUILD_TESTS=OFF",
+           "-DBUILD_DOC=OFF",
+           *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+  end
+
+  test do
+    assert_path_exists include/"mplot/Visual.h"
+  end
+end
