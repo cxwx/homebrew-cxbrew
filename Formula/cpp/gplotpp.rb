ea26381c588a90ee0ad479176e9c592b5e01f45d
cx <6235641+cxwx@users.noreply.github.com>
2026-05-09 22:02:44 +0800

    gplotpp -> cmake

--- Formula/cpp/gplotpp.rb
+++ Formula/cpp/gplotpp.rb
@@ -1,9 +1,20 @@
 class Gplotpp < Formula
-  desc "A header-only C++ library that interfaces with Gnuplot"
+  desc "Header-only C++ library that interfaces with Gnuplot"
   homepage "https://github.com/ziotom78/gplotpp"
   head "https://github.com/ziotom78/gplotpp.git", branch: "master"
 
+  depends_on "cmake" => :build
+
   def install
-    include.install "gplot++.h"
+    system "cmake", "-S", ".", "-B", "build",
+           "-DGPLOTPP_BUILD_EXAMPLES=OFF",
+           "-DGPLOTPP_ENABLE_TESTS=OFF",
+           *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+  end
+
+  test do
+    assert_path_exists include/"gplot++.h"
   end
 end
