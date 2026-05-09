8aec56fc558ad7ad5948252896affc660f01d4a9
cx <6235641+cxwx@users.noreply.github.com>
2026-05-09 14:55:04 +0800

    feat(cpp): add tellenc formula for encoding detection

---
+++ Formula/cpp/tellenc.rb
@@ -0,0 +1,16 @@
+class Tellenc < Formula
+  desc "Program to detect the encoding of a text file"
+  homepage "https://github.com/adah1972/tellenc"
+  head "https://github.com/adah1972/tellenc.git", branch: "master"
+  license "Zlib"
+
+  def install
+    system ENV.cxx, "-O2", "tellenc.cpp", "-o", "tellenc"
+    bin.install "tellenc"
+  end
+
+  test do
+    (testpath/"test.txt").write "Hello, world!"
+    assert_equal "ascii\n", shell_output("#{bin}/tellenc #{testpath}/test.txt")
+  end
+end
