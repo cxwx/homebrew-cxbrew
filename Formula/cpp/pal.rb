c4b0b9963b26ccdc96c1c0254a0e6ea7822ac0a0
cx <6235641+cxwx@users.noreply.github.com>
2026-05-10 04:30:20 +0800

    pal

--- Formula/cpp/pal.rb
+++ Formula/cpp/pal.rb
@@ -1,9 +1,8 @@
 class Pal < Formula
   homepage "https://github.com/IceCube-SPNO/pal"
-#  url "https://github.com/Starlink/pal/releases/download/v0.9.10/pal-0.9.10.tar.gz"
-#  sha256 "191fde8c4f45d6807d4b011511344014966bb46e44029a4481d070cd5e7cc697"
+  # url "https://github.com/Starlink/pal/releases/download/v0.9.10/pal-0.9.10.tar.gz"
+  # sha256 "191fde8c4f45d6807d4b011511344014966bb46e44029a4481d070cd5e7cc697"
   head "https://github.com/cxwx/pal.git", using: :git, branch: "master"
-#  version "0.9.10"
 
   depends_on "erfa"
   # if RUBY_PLATFORM.match?(/darwin/)
@@ -18,8 +17,6 @@ class Pal < Formula
   #   # system "autoreconf" "--fiv"
   #   system "./configure", "--disable-debug",
   #                         "--without-starlink",
-  #                         #                          "--disable-dependency-tracking",
-  #                         #                          "--disable-silent-rules",
   #                         "--prefix=#{prefix}"
   #   system "make", "install"
   # end
