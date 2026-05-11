6baeee4950947744087757dd87bbb8ed9b157aba
cx <6235641+cxwx@users.noreply.github.com>
2026-05-11 16:50:19 +0800

    new formula ( cnl )

---
+++ Formula/cpp/cnl.rb
@@ -0,0 +1,36 @@
+class Cnl < Formula
+  desc "C++ library of fixed-precision numeric classes"
+  homepage "https://github.com/johnmcfarlane/cnl"
+  url "https://github.com/johnmcfarlane/cnl/archive/refs/tags/v1.1.2.tar.gz"
+  sha256 "94c8915917029f3c43f1c7130106077e825ce5d4217bab3472ec3b0f84fe1233"
+  license "BSL-1.0"
+  head "https://github.com/johnmcfarlane/cnl.git", branch: "main"
+
+  depends_on "cmake" => :build
+
+  def install
+    args = %w[
+      -DBUILD_TESTING=OFF
+    ]
+
+    system "cmake", "-S", ".", "-B", "builddir", *args, *std_cmake_args
+    system "cmake", "--build", "builddir"
+    system "cmake", "--install", "builddir"
+  end
+
+  test do
+    (testpath/"test.cpp").write <<~CPP
+      #include <cnl/all.h>
+      #include <iostream>
+
+      int main() {
+        cnl::scaled_integer<int> a{7};
+        cnl::scaled_integer<int> b{3};
+        auto c = a + b;
+        std::cout << "7 + 3 = " << static_cast<int>(c) << std::endl;
+      }
+    CPP
+    system ENV.cxx, "test.cpp", "-std=c++20", "-o", "test"
+    system "./test"
+  end
+end
