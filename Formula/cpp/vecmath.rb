946ac0697a6357dba25c4c80284fb3463e62e163
cx <6235641+cxwx@users.noreply.github.com>
2026-05-16 11:41:26 +0800

    vecmath rewrite

--- Formula/cpp/vecmath.rb
+++ Formula/cpp/vecmath.rb
@@ -1,8 +1,9 @@
 class Vecmath < Formula
   desc "Collection of vectorized based math utilities based on VecCore library"
   homepage "https://github.com/root-project/vecmath"
-  url "https://github.com/root-project/vecmath/archive/v0.1.0.tar.gz"
-  version "0.1.0"
+  url "https://github.com/root-project/vecmath/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "9d032db33a3d88d9c4477b0cc1a1975466cf14e580f305dfa6706b733646f632"
+  license "LGPL-2.1-or-later"
   revision 1
 
   depends_on "cmake" => :build
@@ -10,16 +11,30 @@ class Vecmath < Formula
   depends_on "vdt"
 
   def install
-    mkdir "builddir" do
-      args = std_cmake_args + %w[
-        ../
-        -DROOT=ON
-        -DBACKEND=Vc
-        -DVc_DIR=/usr/local/lib/cmake/Vc/
-        -DBUILD_SHARED_LIBS=ON
-      ]
-      system "cmake", *args
-      system "make", "install"
-    end
+    system "cmake", "-S", ".", "-B", "builddir",
+           "-DBACKEND=Vc",
+           "-DVc_DIR=#{Formula["vc"].opt_lib}/cmake/Vc",
+           "-DBUILD_SHARED_LIBS=ON",
+           *std_cmake_args
+    system "cmake", "--build", "builddir"
+    system "cmake", "--install", "builddir"
+  end
+
+  test do
+    (testpath/"test.cpp").write <<~CPP
+      #include <VecMath/Math.h>
+      #include <cassert>
+      #include <cmath>
+
+      int main() {
+        // Test that the library compiles and links
+        double x = 1.0;
+        double result = vecMath::FastExp(x);
+        assert(std::abs(result - std::exp(x)) < 0.01);
+        return 0;
+      }
+    CPP
+    system ENV.cxx, "test.cpp", "-std=c++14", "-I#{include}", "-I#{Formula["vc"].opt_include}", "-o", "test"
+    system "./test"
   end
 end
