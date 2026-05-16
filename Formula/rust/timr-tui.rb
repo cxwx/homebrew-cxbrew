23d8080a0cfe1a5a07dc2b7687284218d3631888
cx <6235641+cxwx@users.noreply.github.com>
2026-05-16 15:23:38 +0800

    new formula(timr-tui)

---
+++ Formula/rust/timr-tui.rb
@@ -0,0 +1,17 @@
+class TimrTui < Formula
+  desc "TUI to organize your time: Pomodoro, Countdown, Timer, Event"
+  homepage "https://github.com/sectore/timr-tui"
+  url "https://github.com/sectore/timr-tui/archive/refs/tags/v1.8.1.tar.gz"
+  sha256 "3c3c2f5ccf98195ed48ace69abe925077abc2353fc8c601cbb5b08f0d8ad518a"
+  license "MIT"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", *std_cargo_args
+  end
+
+  test do
+    system bin/"timr-tui", "--help"
+  end
+end
