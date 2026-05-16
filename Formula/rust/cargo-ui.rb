9b9a1d49c4bfbbdb6a1b75bba8f8138bd1558241
cx <6235641+cxwx@users.noreply.github.com>
2026-05-16 18:05:43 +0800

    new formula(cargo-ui)

---
+++ Formula/rust/cargo-ui.rb
@@ -0,0 +1,17 @@
+class CargoUi < Formula
+  desc "A GUI for cargo built with the Slint framework"
+  homepage "https://github.com/slint-ui/cargo-ui"
+  url "https://github.com/slint-ui/cargo-ui/archive/refs/tags/v0.3.3.tar.gz"
+  sha256 "79693f5d7bbb2e51c853e295eb5a69673a4ffc109589c6b1637db44563db26d1"
+  license "GPL-3.0-only"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", "--path", ".", "--locked", "--root", prefix.to_s
+  end
+
+  test do
+    # no-op: GUI application
+  end
+end
