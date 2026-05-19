7ccc7d906fcdaa23408b87528e98adf97bcbec72
cx <6235641+cxwx@users.noreply.github.com>
2026-05-19 15:27:52 +0800

    new formula(luaBridge)

---
+++ Formula/cpp/luabridge.rb
@@ -0,0 +1,15 @@
+# cspell:disable
+class Luabridge < Formula
+  desc "Lightweight, dependency-free library for binding Lua to C++"
+  homepage "https://github.com/vinniefalco/LuaBridge"
+
+  url "https://github.com/vinniefalco/LuaBridge/archive/refs/tags/2.10.tar.gz"
+  sha256 "a9bfd72ce041b84b97c9e538e0432ee6511f3673ca71f262301015649a1db2fa"
+
+  license "MIT"
+
+  def install
+    (include/"LuaBridge").install Dir["Source/LuaBridge/*.h"]
+    (include/"LuaBridge/detail").install Dir["Source/LuaBridge/detail/*.h"]
+  end
+end
