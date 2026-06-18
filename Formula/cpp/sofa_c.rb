b9e88d3a9cd3a78c98ea0a9164c3390981eed775
cx <6235641+cxwx@users.noreply.github.com>
2026-06-18 22:15:36 +0800

    sofa_c update

--- Formula/cpp/sofa_c.rb
+++ Formula/cpp/sofa_c.rb
@@ -3,9 +3,17 @@ class SofaC < Formula
   homepage "http://www.iausofa.org/"
   url "https://static1.squarespace.com/static/68caa251ae552a6213e8764b/t/68d3f1e498fb781111a023da/1758720484117/sofa_c-20231011.tar.gz"
   sha256 "d9c10833cae8b4d9361a0ffda31ec361fd1262362025bec4d4e51a880150ace2"
+  # SOFA 用日期版本,上游官网记法为 2023-10-11;url 文件名/tarball 目录紧凑为 20231011。
+  # 这里显式 version 用带横线记法以与 livecheck(官网页面)对齐。
+  version "2023-10-11"
+
+  livecheck do
+    url "http://www.iausofa.org/current-software"
+    regex(%r{href="/(\d{4}-\d{2}-\d{2})c"}i)
+  end
 
   def install
-    cd "20231011/c/src" do
+    cd "#{version.to_s.delete('-')}/c/src" do
       # Original makefile uses `ar ru` to build static library.
       # Replace with shared library build for both macOS and Linux.
       inreplace "makefile", "ar ru", "gcc -fPIC -shared -o"
