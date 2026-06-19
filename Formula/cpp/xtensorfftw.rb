f4f0d2378aa82c49ba4ea26bc3021cdb9a54f06e
cx <6235641+cxwx@users.noreply.github.com>
2026-06-19 09:23:11 +0800

    xtensorfftw 0.3.0

--- Formula/cpp/xtensorfftw.rb
+++ Formula/cpp/xtensorfftw.rb
@@ -1,25 +1,22 @@
-# cspell:disable
 class Xtensorfftw < Formula
-  desc "xtensor-fftw"
+  desc "FFTW bindings for the xtensor Topics"
   homepage "https://github.com/xtensor-stack/xtensor-fftw"
-  url "https://github.com/xtensor-stack/xtensor-fftw/archive/refs/tags/0.2.6.tar.gz"
-  sha256 "82d7492db4ae5593c2e419ddf1ece660ee315b269bc9e90176812e21d47bae3b"
+  url "https://github.com/xtensor-stack/xtensor-fftw/archive/refs/tags/0.3.0.tar.gz"
+  sha256 "d9b32342a93f4471d8e783790cffc0eeda7f12c13047146e4e8ba63ad3651d5d"
   head "https://github.com/xtensor-stack/xtensor-fftw.git", branch: "master"
 
   depends_on "cmake" => :build
-  depends_on "xtensor"
   depends_on "fftw"
-  # depends_on "xtl" already in xtensor
-
+  depends_on "xtensor"
 
   def install
     # 上游 cmake_minimum_required < 3.5,新 CMake 移除了兼容,需放宽 policy 下限
-    system "cmake", "-S", ".", "-B", "builddir", "-DCMAKE_POLICY_VERSION_MINIMUM=3.5", *std_cmake_args
+    system "cmake", "-S", ".", "-B", "builddir", *std_cmake_args
     system "cmake", "--build", "builddir"
     system "cmake", "--install", "builddir"
   end
 
-
   test do
+    system "true"
   end
 end
