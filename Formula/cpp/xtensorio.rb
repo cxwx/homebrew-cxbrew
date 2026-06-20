8baa191da6cd83cbcf170a4206a9563aa0e403aa
cx <6235641+cxwx@users.noreply.github.com>
2026-06-21 00:02:01 +0800

    workflow fix

--- Formula/cpp/xtensorio.rb
+++ Formula/cpp/xtensorio.rb
@@ -2,8 +2,8 @@
 class Xtensorio < Formula
   desc "C++ library for reading/writing xtensor data formats"
   homepage "https://github.com/xtensor-stack/xtensor-io"
-  url "https://github.com/xtensor-stack/xtensor-io/archive/refs/tags/0.13.0.tar.gz"
-  sha256 "470bedee082adb0ef25ef7b54f9cfd3684e27b8489c42cf7980e0d90c14d04da"
+  url "https://github.com/xtensor-stack/xtensor-io/archive/refs/tags/0.14.0.tar.gz"
+  sha256 "6aceb66e32a88eff58028443eacc1e73b6453d36c57eb4f2f13925356bbe5947"
   head "https://github.com/xtensor-stack/xtensor-io.git", branch: "master"
 
   depends_on "cmake" => :build
