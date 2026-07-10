6bc7a35dd3fb83f3a71220fabc9ce9a2721be923
cx <6235641+cxwx@users.noreply.github.com>
2026-07-10 09:32:01 +0800

    fix c23

--- Formula/c/zathura.rb
+++ Formula/c/zathura.rb
@@ -8,20 +8,38 @@ class Zathura < Formula
   depends_on "meson" => :build
   depends_on "ninja" => :build
   depends_on "pkg-config" => :build
+  depends_on "cairo"
   depends_on "gettext"
   depends_on "girara"
   depends_on "glib"
-  depends_on "gtk+3"
+  depends_on "gtk4"
   depends_on "json-glib"
   depends_on "libmagic"
   depends_on "sqlite"
+  depends_on "xxhash"
 
   on_macos do
-    depends_on "gtk-mac-integration"
+    # zathura jumps `goto` across g_autofree (__attribute__((cleanup))) variables,
+    # which Clang rejects; build with GCC instead.
+    depends_on "gcc" => :build
   end
 
   def install
-    system "meson", "setup", "build", "-Dsynctex=disabled", "-Dseccomp=disabled",
+    ENV["CC"] = (formula_opt_bin("gcc")/"gcc-#{Formula["gcc"].version.major}").to_s if OS.mac?
+
+    # Upstream ships a stray </issue> where </p> is meant in the release notes.
+    inreplace "data/org.pwmt.zathura.metainfo.xml.in",
+              "test-wayland features</issue>",
+              "test-wayland features</p>"
+    # gettext 1.0 hides the AppStream ITS rules from msgfmt, breaking the
+    # metainfo translation merge; install the file untranslated instead.
+    inreplace "data/meson.build" do |s|
+      s.gsub! "appdata = i18n.merge_file(", "appdata = configure_file("
+      s.gsub! "  po_dir: podir,\n)", "  copy: true,\n)"
+    end
+
+    # Upstream targets C23; older toolchains only spell the dialect `c2x`/`gnu2x`.
+    system "meson", "setup", "build", "-Dc_std=gnu2x", "-Dsynctex=disabled", "-Dseccomp=disabled",
            "-Dlandlock=disabled", "-Dmanpages=disabled", *std_meson_args
     system "meson", "compile", "-C", "build"
     system "meson", "install", "-C", "build"
